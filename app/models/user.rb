1636be328d33f24a0e9c3e8a4cd3d09dbaf20ae9
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-07 16:16:51 -0500

    made users controller, completed routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,28 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
   validates :email, format: { with: /\S{2,}@\S{2,}\.\S{2,}/ }
   validates :email, presence: true, uniqueness: true
 
   has_many :urls
 
-  # include BCrypt
+  include BCrypt
+
+  def password=(secret)
+    return if secret == ''
+    @password = Password.create(secret)
+    self.password_digest = @password
+  end
+
+  def password
+    return unless password_digest
+    @password ||= Password.new(self.password_digest)
+  end
+
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    return user if user && (user.password == password)
+    nil
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 UrlShortener::Application.routes.draw do
+
+  resources :users, except: :index
+  resources :urls, only: :create
+
+  root "users#index"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +47,7 @@ UrlShortener::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
