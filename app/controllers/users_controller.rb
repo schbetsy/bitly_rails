6d27d58fc77b629f6be712a51648cd756098e976
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-07 17:03:05 -0500

    helper method for current user, some views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def index
+    @user = User.new
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      reset_session
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
