552fea4729448c5a74e69c903d3f4728fed9c465
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-07 19:00:37 -0500

    add controller action for creating url

--- app/controllers/concerns/urls_controller.rb
+++ app/controllers/concerns/urls_controller.rb
@@ -1,3 +1,11 @@
 class UrlsController < ApplicationController
+  def create
+    url = Url.new(params[:url])
+    if url.save
+      redirect_to user_path(current_user.id)
+    else
+      redirect_to user_path(current_user.id)
+    end
 
+  end
 end
